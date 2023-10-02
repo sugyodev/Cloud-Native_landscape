0f4e482c02b985320f7954e99e9b21a01b41f069
sugyodevm <sg.guru1030@gmail.com>
2023-10-02 18:48:46 +1000

    updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,16 +8,42 @@ const MAX_LG = 12
 function Dashboard() {
   const [showSidebar, setShowSidebar] = useState(false)
   const [year, setYear] = useState('')
+  const [label, setLabel] = useState('')
 
-  const filteredData = (data, key, value) => {
-    if (!data) return []
-    if (value && key) {
-      return data.filter(val => val[key] == value)
+  const isFilterMatched = (y_val = '', l_val = '') => {
+    const isLabelMatched = l_val.toUpperCase().indexOf(label.toUpperCase()) > -1
+    const isYearMatched = y_val.toString().indexOf(year.toString()) > -1
+
+    if (year && label) {
+      return isYearMatched && isLabelMatched
+    } else if (year) {
+      return isYearMatched
+    } else if (label) {
+      return isLabelMatched
     } else {
-      return data
+      return false
     }
   }
 
+  const drawFlow = (jsonData) => (
+    jsonData.map((val, ind) => (
+      <div key={ind} className='p-4 h-fit w-full sm:w-fit border-[#24bdae] border min-w-[100px] m-1 rounded my-2' style={{ flex: '1 0 auto' }}>
+        <h2 className={`${val?.subgraph ? 'sm:text-lg text-base text-gray-400' : 'sm:text-base text-[15px] text-gray-500'} font-semibold text-center uppercase`}>{val?.label}</h2>
+        {val?.subset &&
+          <div className={`${val?.subset?.length > MAX_SM ? 'mx-auto flex flex-wrap gap-4 justify-center max-w-[350px]' : ''} ${val?.subset?.length > MAX_MD ? 'md:max-w-[450px]' : ''}  ${val?.subset?.length > MAX_LG ? 'md:max-w-[600px]' : ''} mt-4`}>
+            {val?.subset.map((subData, no) => (
+              <div key={no} className='my-2 text-gray-800 font-semibold text-sm w-fit text-center mx-auto'>
+                <img src={subData?.icon} className='max-w-[250px] sm:max-w-auto h-auto w-auto max-h-[80px] min-h-[50px]' alt='landscape' />
+                <p className={`${isFilterMatched(subData?.year, subData?.label) && 'bg-[#00ffaaa6]'} w-fit mx-auto`}>{subData?.label}</p>
+              </div>
+            ))
+            }
+          </div>
+        }
+        {val?.subgraph && <div className='flex flex-wrap'>{drawFlow(val?.subgraph)}</div>}
+      </div>
+    ))
+  )
   return (
     <div className="dashboard w-full min-h-screen h-fit">
       <div className={`shadow-right w-[300px] min-h-screen h-fit left-0 pt-12 p-6 z-[2] absolute bg-white transition-all duration-300 shrink-0 ${showSidebar ? "block" : "hidden md:block"}`}>
@@ -28,12 +54,21 @@ function Dashboard() {
           <h1 className='text-gray-600 text-xl sm:text-2xl font-semibold'>Filters</h1>
         </div>
         <div className='py-4 mt-2'>
-          <div className='flex gap-2 items-center text-gray-500'>
-            <p>Year&nbsp;:</p>
+          <div className='flex gap-2 items-center text-gray-700'>
+            <p className='w-16'>Year&nbsp;:</p>
             <input
+              value={year}
               onChange={(e) => setYear(e.target.value)}
               type="number"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:border-gray-500 block w-full p-2.5" placeholder="2020" />
+              className="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:outline-none focus:border-gray-700 block w-full p-2.5" placeholder="2020" />
+          </div>
+          <div className='flex gap-2 items-center text-gray-700 mt-2'>
+            <p className='w-16'>Label&nbsp;:</p>
+            <input
+              value={label}
+              onChange={(e) => setLabel(e.target.value)}
+              type="text"
+              className="bg-gray-50 border border-gray-400 text-gray-900 text-sm rounded-lg focus:outline-none focus:border-gray-700 block w-full p-2.5" placeholder="Type label ..." />
           </div>
         </div>
       </div>
@@ -45,19 +80,7 @@ function Dashboard() {
         </div>
         <div className='border mt-4 runded-lg flex flex-wrap border-gray-300 px-4 sm:px-8 py-4 w-fit max-w-[100%] rounded-lg justify-center'>
           {
-            DummyJson.map((val, ind) => (
-              <div key={ind} className='p-4 h-fit w-full sm:w-fit border-[#24bdae] border min-w-[100px] m-1 rounded my-2' style={{ flex: '1 0 auto' }}>
-                <h2 className='sm:text-lg text-gray-400 font-semibold text-center uppercase'>{val?.label}</h2>
-                <div className={`${val?.subset?.length > MAX_SM ? 'mx-auto flex flex-wrap gap-4 justify-center max-w-[350px]' : ''} ${val?.subset?.length > MAX_MD ? 'md:max-w-[450px]' : ''}  ${val?.subset?.length > MAX_LG ? 'md:max-w-[600px]' : ''} mt-4`}>
-                  {val?.subset.map((subData, no) => (
-                    <div key={no} className='my-2 text-gray-600 font-semibold text-sm w-fit text-center mx-auto'>
-                      <img src={subData?.icon} className='max-w-[250px] sm:max-w-auto h-auto w-auto max-h-[80px] min-h-[50px]' alt='landscape' />
-                      <p className={`${year == subData?.year && 'bg-[#00ffaaa6]'} w-fit mx-auto`}>{subData?.label}</p>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            ))
+            drawFlow(DummyJson)
           }
         </div>
       </div>
--- src/pages/Dummy.const.js
+++ src/pages/Dummy.const.js
@@ -2,61 +2,58 @@ const DummyJson = [
     {
         "label": "genAI",
         "subset": [
-            { "label": "openAI", "icon": "https://1000logos.net/wp-content/uploads/2023/02/ChatGPT-Logo-tumb.png", "year": 2022 },
-            { "label": "Automattic", "icon": "https://1000logos.net/wp-content/uploads/2023/08/New-Flyer-Industries-Logo-tumb.png", "year": 2020 },
+            { "label": "openAI", "icon": "https://1000logos.net/wp-content/uploads/2023/09/FNAF-Security-Breach-Logo-tumb.png", "year": 2022 },
+            { "label": "Automattic", "icon": "https://1000logos.net/wp-content/uploads/2023/02/ChatGPT-Logo-tumb.png", "year": 2020 },
             { "label": "Huggingface", "icon": "https://1000logos.net/wp-content/uploads/2023/09/Tauro-Sport-Auto-Logo-tumb.png", "year": 2020 },
-            { "label": "Mayo Clinic", "icon": "https://media-thumbs.golden.com/vAia2Fxzy-WdLfgf_BStxYB3TWk=/30x30/smart/golden-storage-production.s3.amazonaws.com%2Ftopic_images%2F71a8a9df6e284d7d976aa0917bd37d9f.png", "year": 2020 },
-            { "label": "Midjourney", "icon": "https://media-thumbs.golden.com/QNllWS2OPtp9M31w1U-rFocz2Cs=/30x30/smart/golden-storage-production.s3.amazonaws.com%2Ftopic_images%2Fc59bb0465e7c46419225ba328fa9153e.png", "year": 2020 },
+            { "label": "Mayo Clinic", "icon": "https://1000logos.net/wp-content/uploads/2017/01/Adidas-Logo.jpg", "year": 2020 },
+            { "label": "Midjourney", "icon": "https://1000logos.net/wp-content/uploads/2019/06/Real-Madrid-Logo.jpg", "year": 2020 },
+            { "label": "deepmind", "icon": "https://1000logos.net/wp-content/uploads/2023/08/New-Flyer-Industries-Logo-tumb.png", "year": 2020 }
         ]
     }, {
         "label": "Data Integration",
         "subset": [
-            { "label": "Collibra", "icon": "https://1000logos.net/wp-content/uploads/2017/06/Seattle-Seahawks-Logo-tm.jpg", "year": 2020 },
-            { "label": "Infomatica", "icon": "https://1000logos.net/wp-content/uploads/2023/09/Thairung-Logo-tumb.png", "year": 2022 },
-            { "label": "MuleSoft", "icon": "https://1000logos.net/wp-content/uploads/2018/09/Honda-logo-500x281.jpg", "year": 2020 }
+            { "label": "Collibra", "icon": "https://1000logos.net/wp-content/uploads/2018/09/Honda-logo-500x281.jpg", "year": 2020 },
+            { "label": "Infomatica", "icon": "https://1000logos.net/wp-content/uploads/2023/09/FNAF-Security-Breach-Logo-tumb.png", "year": 2022 },
+            { "label": "MuleSoft", "icon": "https://1000logos.net/wp-content/uploads/2023/09/Tauro-Sport-Auto-Logo-tumb.png", "year": 2020 }
         ]
     }, {
         "label": "Business Intelligence",
         "subset": [
-            { "label": "PowerBI", "icon": "https://1000logos.net/wp-content/uploads/2023/09/FNAF-Security-Breach-Logo-tumb.png", "year": 2021 },
-            { "label": "Tableau", "icon": "https://1000logos.net/wp-content/uploads/2017/01/Adidas-Logo.jpg", "year": 2020 },
-            { "label": "Sisense", "icon": "https://media-thumbs.golden.com/xaOCVZl3wgEYnaFbqmrvEzFrzZ8=/30x30/smart/golden-storage-production.s3.amazonaws.com%2Ftopic_images%2F2b3bca3043614ead8740aed7e7579575.jpeg", "year": 2021 },
-            { "label": "ThoughtSpot", "icon": "https://1000logos.net/wp-content/uploads/2019/06/Real-Madrid-Logo.jpg", "year": 2020 }
+            { "label": "PowerBI", "icon": "https://1000logos.net/wp-content/uploads/2017/06/Seattle-Seahawks-Logo-tm.jpg", "year": 2021 },
+            { "label": "Tableau", "icon": "https://1000logos.net/wp-content/uploads/2018/09/Honda-logo-500x281.jpg", "year": 2020 },
+            { "label": "Sisense", "icon": "https://1000logos.net/wp-content/uploads/2023/09/Tauro-Sport-Auto-Logo-tumb.png", "year": 2021 },
+            { "label": "ThoughtSpot", "icon": "https://1000logos.net/wp-content/uploads/2017/01/Adidas-Logo.jpg", "year": 2020 }
         ]
     },
     {
-        "label": "genAI",
-        "subset": [
-            { "label": "Mayo Clinic", "icon": "https://media-thumbs.golden.com/vAia2Fxzy-WdLfgf_BStxYB3TWk=/30x30/smart/golden-storage-production.s3.amazonaws.com%2Ftopic_images%2F71a8a9df6e284d7d976aa0917bd37d9f.png", "year": 2020 },
-            { "label": "Midjourney", "icon": "https://media-thumbs.golden.com/QNllWS2OPtp9M31w1U-rFocz2Cs=/30x30/smart/golden-storage-production.s3.amazonaws.com%2Ftopic_images%2Fc59bb0465e7c46419225ba328fa9153e.png", "year": 2020 },
-            { "label": "openAI", "icon": "https://1000logos.net/wp-content/uploads/2023/02/ChatGPT-Logo-tumb.png", "year": 2022 },
-            { "label": "deepmind", "icon": "https://1000logos.net/wp-content/uploads/2017/10/Skittles-logo-tumb.jpg", "year": 2020 },
-            { "label": "Tableau", "icon": "https://1000logos.net/wp-content/uploads/2017/01/Adidas-Logo.jpg", "year": 2020 },
-            { "label": "Huggingface", "icon": "https://1000logos.net/wp-content/uploads/2023/09/Tauro-Sport-Auto-Logo-tumb.png", "year": 2020 },
-            { "label": "Sisense", "icon": "https://media-thumbs.golden.com/xaOCVZl3wgEYnaFbqmrvEzFrzZ8=/30x30/smart/golden-storage-production.s3.amazonaws.com%2Ftopic_images%2F2b3bca3043614ead8740aed7e7579575.jpeg", "year": 2021 },
-            { "label": "ThoughtSpot", "icon": "https://1000logos.net/wp-content/uploads/2019/06/Real-Madrid-Logo.jpg", "year": 2020 },
-            { "label": "Automattic", "icon": "https://1000logos.net/wp-content/uploads/2023/08/New-Flyer-Industries-Logo-tumb.png", "year": 2020 },
-            { "label": "Mayo Clinic", "icon": "https://media-thumbs.golden.com/vAia2Fxzy-WdLfgf_BStxYB3TWk=/30x30/smart/golden-storage-production.s3.amazonaws.com%2Ftopic_images%2F71a8a9df6e284d7d976aa0917bd37d9f.png", "year": 2020 },
-            { "label": "Midjourney", "icon": "https://media-thumbs.golden.com/QNllWS2OPtp9M31w1U-rFocz2Cs=/30x30/smart/golden-storage-production.s3.amazonaws.com%2Ftopic_images%2Fc59bb0465e7c46419225ba328fa9153e.png", "year": 2020 },
-            { "label": "openAI", "icon": "https://1000logos.net/wp-content/uploads/2023/02/ChatGPT-Logo-tumb.png", "year": 2022 },
-            { "label": "deepmind", "icon": "https://1000logos.net/wp-content/uploads/2017/10/Skittles-logo-tumb.jpg", "year": 2020 },
-            { "label": "Tableau", "icon": "https://1000logos.net/wp-content/uploads/2017/01/Adidas-Logo.jpg", "year": 2020 },
-            { "label": "Huggingface", "icon": "https://1000logos.net/wp-content/uploads/2023/09/Tauro-Sport-Auto-Logo-tumb.png", "year": 2020 },
-            { "label": "Sisense", "icon": "https://media-thumbs.golden.com/xaOCVZl3wgEYnaFbqmrvEzFrzZ8=/30x30/smart/golden-storage-production.s3.amazonaws.com%2Ftopic_images%2F2b3bca3043614ead8740aed7e7579575.jpeg", "year": 2021 },
-            { "label": "ThoughtSpot", "icon": "https://1000logos.net/wp-content/uploads/2019/06/Real-Madrid-Logo.jpg", "year": 2020 },
-            { "label": "Automattic", "icon": "https://1000logos.net/wp-content/uploads/2023/08/New-Flyer-Industries-Logo-tumb.png", "year": 2020 },
+        "label": "Data Science",
+        "subgraph": [
+            {
+                "label": "Foo Bar",
+                "subset": [
+                    { "label": "openAI", "icon": "https://1000logos.net/wp-content/uploads/2019/06/Real-Madrid-Logo.jpg", "year": 2022 },
+                    { "label": "Automattic", "icon": "https://1000logos.net/wp-content/uploads/2023/02/ChatGPT-Logo-tumb.png", "year": 2020 },
+                    { "label": "Huggingface", "icon": "https://1000logos.net/wp-content/uploads/2017/01/Adidas-Logo.jpg", "year": 2020 },
+                    { "label": "Midjourney", "icon": "https://1000logos.net/wp-content/uploads/2019/06/Real-Madrid-Logo.jpg", "year": 2023 },
+                ]
+            }, {
+                "label": "Lores Ipsum",
+                "subset": [
+                    { "label": "Collibra", "icon": "https://1000logos.net/wp-content/uploads/2018/09/Honda-logo-500x281.jpg", "year": 2020 },
+                    { "label": "Infomatica", "icon": "https://1000logos.net/wp-content/uploads/2023/09/FNAF-Security-Breach-Logo-tumb.png", "year": 2023 },
+                ]
+            }, {
+                "label": "Random Forest",
+                "subset": [
+                    { "label": "PowerBI", "icon": "https://1000logos.net/wp-content/uploads/2017/06/Seattle-Seahawks-Logo-tm.jpg", "year": 2021 },
+                    { "label": "Tableau", "icon": "https://1000logos.net/wp-content/uploads/2018/09/Honda-logo-500x281.jpg", "year": 2020 },
+                    { "label": "Sisense", "icon": "https://1000logos.net/wp-content/uploads/2023/09/Tauro-Sport-Auto-Logo-tumb.png", "year": 2021 },
+                    { "label": "ThoughtSpot", "icon": "https://1000logos.net/wp-content/uploads/2017/01/Adidas-Logo.jpg", "year": 2020 },
+                    { "label": "MuleSoft", "icon": "https://1000logos.net/wp-content/uploads/2023/09/Tauro-Sport-Auto-Logo-tumb.png", "year": 2020 }
+                ]
+            }
         ]
-    },
-    {
-        "label": "Business Intelligence",
-        "subset": [
-            { "label": "Tableau", "icon": "https://1000logos.net/wp-content/uploads/2017/01/Adidas-Logo.jpg", "year": 2020 },
-            { "label": "Sisense", "icon": "https://media-thumbs.golden.com/xaOCVZl3wgEYnaFbqmrvEzFrzZ8=/30x30/smart/golden-storage-production.s3.amazonaws.com%2Ftopic_images%2F2b3bca3043614ead8740aed7e7579575.jpeg", "year": 2021 },
-            { "label": "ThoughtSpot", "icon": "https://1000logos.net/wp-content/uploads/2019/06/Real-Madrid-Logo.jpg", "year": 2020 },
-            { "label": "PowerBI", "icon": "https://1000logos.net/wp-content/uploads/2023/09/FNAF-Security-Breach-Logo-tumb.png", "year": 2021 },
-        ]
-    },
-    
+    }
 ]
 
 export default DummyJson
